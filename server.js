122f6840a518e795bed597ac81e61e8894a83365
Allen <allen0321.lin@ehsn.com.tw>
2020-09-17 14:41:19 +0800

    處理CORS問題

--- server.js
+++ server.js
@@ -2,23 +2,7 @@ const express = require("express");
 const port = process.env.PORT || 3000;
 const app = express();
 
-const allowedOrigins = ["*"];
 const Ticker = require("./data.json");
-
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        var msg =
-          "The CORS policy for this site does not " +
-          "allow access from the specified Origin.";
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    },
-  })
-);
 
 app.get("/", (req, res) => {
   res.send("working");
