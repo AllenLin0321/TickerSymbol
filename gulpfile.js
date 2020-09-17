485843ea9931fe0ad30de9c12ab111a96a3b2113
Allen <allen0321.lin@ehsn.com.tw>
2020-09-17 11:35:24 +0800

    添加Gulp 處理 原始 JSON 檔案

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+﻿const gulp = require("gulp");
+const jsonTransform = require("gulp-json-transform");
+
+gulp.task("format", function () {
+  return gulp
+    .src("./src/data.json")
+    .pipe(
+      jsonTransform(function (data, file) {
+        return data.map(stock => ({
+          symbol: stock.symbol,
+          name: stock.name,
+        }));
+      })
+    )
+    .pipe(gulp.dest("./"));
+});
--- server.js
+++ server.js
@@ -1,33 +1,28 @@
-const express = require('express')
+const express = require("express");
+const port = process.env.PORT || 3000;
+const app = express();
 
-const Ticker = require('./data')
-const port = process.env.PORT || 3000
-const app = express()
+var Ticker = require("./data.json");
 
+app.get("/", (req, res) => {
+  res.send("working");
+  res.end();
+});
 
+app.get("/keyword/:search", (req, res) => {
+  console.log("--------/keyword-----------" + req.params.search);
+  let search = req.params.search;
+  search = search.toLowerCase();
+  res.setHeader("Content-Type", "application/json");
+  const g = Ticker.filter(
+    f => JSON.stringify(f).toLowerCase().indexOf(search) !== -1
+  );
 
-app.get('/', (req, res) => {
-    
-    res.send('working')
-
-    res.end()
-})
-
-
-app.get('/keyword/:search', (req, res) => {
-    console.log("--------/keyword-----------"+req.params.s)
-    let search = req.params.search;
-    search = search.toLowerCase();
-    res.setHeader('Content-Type', 'application/json');
-    const g  =Ticker.TickerSymbols.filter( f => 
-        (JSON.stringify(f).toLowerCase().indexOf(search) !== -1)
-       )
-
-    res.send(g)
-    res.end()
-})
+  res.send(g);
+  res.end();
+});
 
 app.listen(port, err => {
-    if (err) throw err
-    console.log(`> Ready On Server http://localhost:${port}`)
-})
+  if (err) throw err;
+  console.log(`> Ready On Server http://localhost:${port}`);
+});
